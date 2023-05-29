70e7c53ab82ae950097f3dca418f418ab7dbb88a
Tizio a caso <gullottidiego2@gmail.com>
2023-05-29 23:31:41 +0200

    pathfinding_ai_NPC

---
+++ Assets/_Game/Script/Vista_Top_Down/NPC/NPCAI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAI : MonoBehaviour
+{
+    /*private EnemyPathfindingMovement PathfindingMovement;
+    private Vector3 startingPosition;
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }*/
+
+    
+}
