7cfe388e0dad5df90c74faf5dcdef37e533f5b84
Tizio a caso <gullottidiego2@gmail.com>
2023-06-05 16:13:04 +0200

    init commit

---
+++ Assets/_Game/Script/Vista_Top_Down/NPC/ChairStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairStatus : MonoBehaviour
+{
+
+    public int chairNumber;
+    public bool isOccupied = false;
+}
+
