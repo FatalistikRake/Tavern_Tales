35aeb64f6c6c06cb56f58a40c71c5efbc7189681
feddde <federico.amadio.2006@gmail.com>
2023-06-18 14:35:17 +0200

    .

--- Assets/_Game/Script/Vista_Top_Down/Player/Inventory/UI_Scripts/StaticInventoryDisplay.cs
+++ Assets/_Game/Script/Vista_Top_Down/Player/Inventory/UI_Scripts/StaticInventoryDisplay.cs
@@ -1,49 +1,49 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class StaticInventoryDisplay : InventoryDisplay
-{
-    [SerializeField] private InventoryHolder inventoryHolder;
-    [SerializeField] private InventorySlot_UI[] slots;
+//public class StaticInventoryDisplay : InventoryDisplay
+//{
+//    [SerializeField] private InventoryHolder inventoryHolder;
+//    [SerializeField] private InventorySlot_UI[] slots;
 
-    private void OnEnable()
-    {
-        PlayerInventoryHolder.OnPlayerInventoryChanged += RefreshStaticDisplay;
-    }
+//    private void OnEnable()
+//    {
+//        PlayerInventoryHolder.OnPlayerInventoryChanged += RefreshStaticDisplay;
+//    }
 
-    private void OnDisable()
-    {
-        PlayerInventoryHolder.OnPlayerInventoryChanged -= RefreshStaticDisplay;
-    }
+//    private void OnDisable()
+//    {
+//        PlayerInventoryHolder.OnPlayerInventoryChanged -= RefreshStaticDisplay;
+//    }
 
-    private void RefreshStaticDisplay()
-    {
-        if (inventoryHolder != null)
-        {
-            inventorySystem = inventoryHolder.PrimaryInventorySystem;
-            inventorySystem.OnInventorySlotChanged += UpdateSlot;
-        }
-        else Debug.LogWarning($"No inventory assigned to {gameObject.name}");
+//    private void RefreshStaticDisplay()
+//    {
+//        if (inventoryHolder != null)
+//        {
+//            inventorySystem = inventoryHolder.PrimaryInventorySystem;
+//            inventorySystem.OnInventorySlotChanged += UpdateSlot;
+//        }
+//        else Debug.LogWarning($"No inventory assigned to {gameObject.name}");
 
-        AssignSlot(inventorySystem, 8);
-    }
+//        AssignSlot(inventorySystem, 8);
+//    }
 
-    protected override void Start()
-    {
-        base.Start();
+//    protected override void Start()
+//    {
+//        base.Start();
 
-        RefreshStaticDisplay();
-    }
+//        RefreshStaticDisplay();
+//    }
 
-    public override void AssignSlot(InventorySystem invToDisplay, int offset)
-    {
-        slotDictionary = new Dictionary<InventorySlot_UI, InventorySlot>();
+//    public override void AssignSlot(InventorySystem invToDisplay, int offset)
+//    {
+//        slotDictionary = new Dictionary<InventorySlot_UI, InventorySlot>();
 
-        for (int i = 0; i < slots.Length; i++)
-        {
-            slotDictionary.Add(slots[i], inventorySystem.InventorySlots[i]);
-            slots[i].Init(inventorySystem.InventorySlots[i]);
-        }
-    }
-}
+//        for (int i = 0; i < slots.Length; i++)
+//        {
+//            slotDictionary.Add(slots[i], inventorySystem.InventorySlots[i]);
+//            slots[i].Init(inventorySystem.InventorySlots[i]);
+//        }
+//    }
+//}
